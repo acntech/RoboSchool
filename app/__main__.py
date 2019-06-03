bff6299ce5dea908bb353855cc06ef856606b2e2
Fabian Sødal Dietrichson <fabiansd1402@gmail.com>
2019-06-03 09:44:10 +0200

    app implemented, docker images created

---
+++ app/__main__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import render_template
+import os
+
+app = Flask(__name__)
+
+
+###########
+### APP ###
+###########
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    ENVIRONMENT_DEBUG = os.environ.get("DEBUG", False)
+    app.run(host='0.0.0.0', port=9000, debug=ENVIRONMENT_DEBUG)
