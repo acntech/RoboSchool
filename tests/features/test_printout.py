b87bfaf7b14c92604ad43bc789579b66012df47e
Fabian Sødal Dietrichson <fabiansd1402@gmail.com>
2019-08-27 14:17:42 +0200

    updated docker setup to work with gym

--- tests/features/test_printout.py
+++ tests/features/test_printout.py
@@ -1,23 +1,26 @@
-import unittest
+import pytest
 
 from src.features.printout import print_image
 from src.features.printout import add
 
 
-class TestPrintout(unittest.TestCase):
+def test_printout():
+    test_return_message = print_image()
+    assert test_return_message == "successful print"
 
-    def setUp(self):
-        self.return_message = "successful print"
-        self.inn_a = 2
-        self.inn_b = 3
 
-    def test_print_images(self):
-        test_return_message = print_image()
-        self.assertEqual(self.return_message, test_return_message)
+@pytest.fixture
+def give_numbers(request):
+    a = 2
+    b = 3
+    yield a, b
 
-    def test_add(self):
-        add_return_value = add(self.inn_a, self.inn_b)
-        self.assertEqual(add_return_value, 5)
+    def tear_down():
+        print('Delete the files')
 
-if __name__ == '__main__':
-    unittest.main()
+    print(a, b)
+
+
+def test_add(give_numbers):
+    a, b = give_numbers
+    assert add(a, b) == 5
