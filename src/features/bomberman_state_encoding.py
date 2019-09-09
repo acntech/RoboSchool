ce51a6ff06c850e6d836d8d22fbd73b27d08b243
Lasse Henriksen <lassehhenriksen@gmail.com>
2019-09-09 12:34:07 +0200

    Move information about the agent position to the adversary image (renamed from enemy image) and scale all values -1 to 1

--- src/features/bomberman_state_encoding.py
+++ src/features/bomberman_state_encoding.py
@@ -7,7 +7,7 @@ BOARD_WIDTH = 15
 
 # TODO: Load in a config file or something instead of having hard coded constants inside
 # TODO: Send inn a dummy board and check for correctness (Visualize with ascii art, board class)
-# TODO: Add powerup information (number of bombs and players speeds)
+# TODO: Add powerup information (number of bombs the agent has left and players speeds)
 
 # Nice to have todos:
 # TODO: Figure out a way to encode movement (aka player 2 is moving west even though he is standing in position x right now)
@@ -15,15 +15,15 @@ BOARD_WIDTH = 15
 
 # LAYERS:
 # - Fire and danger (e.g 0 when bomb is placed -> progress to -1 as it approaches explosion, 1 for safe)
-# - Agent, crates, bonus, empty tile (0 for empty, 1 for crate, 2 for bonus, 3 for player)
-# - Enemy players (0 for empty tiles, 1 for enemy)
+# - Agent, crates, bonus, empty tile (-1 for bonus, 0 for empty, 1 for create)
+# - Players and enemy players (-1 for enemy, 0 for empty tiles, 1 for player)
 # - Powerups ...
 
-def construct_images(bombs, fires, walls, player, enemies, crates, bonuses):
+def construct_images(bombs, fires, walls, agent, enemies, crates, bonuses):
     danger_image = construct_danger_image(bombs, fires, walls)
-    player_and_env_image = construct_player_and_env_image(player, bonuses, crates, walls)
-    enemy_image = construct_enemy_image(enemies, walls)
-    state_vector = np.concatenate((danger_image, player_and_env_image, enemy_image))
+    env_image = construct_env_image(bonuses, crates, walls)
+    adversary_image = construct_adversary_image(agent, enemies, walls)
+    state_vector = np.concatenate((danger_image, env_image, adversary_image))
     return state_vector
 
 def construct_danger_image(bombs, fires, walls):
@@ -51,13 +51,12 @@ def construct_danger_image(bombs, fires, walls):
 
     return danger_image_flatten
 
-def construct_player_and_env_image(player, bonuses, crates, walls):
+def construct_env_image(bonuses, crates, walls):
     player_and_env_image = np.zeros(BOARD_HEIGHT, BOARD_WIDTH)
     for crate in crates:
         player_and_env_image[crate.position.x, crate.position.y] = 1
     for bonus in bonuses:
-        player_and_env_image[bonus.position.x, bonus.position.y] = 2
-    player_and_env_image[player.position.x, player.position.y] = 3
+        player_and_env_image[bonus.position.x, bonus.position.y] = -1
 
     # Flatten layer and remove wall tiles
     player_and_env_image_flatten = player_and_env_image.flatten()
@@ -65,14 +64,18 @@ def construct_player_and_env_image(player, bonuses, crates, walls):
 
     return player_and_env_image_flatten
 
-def construct_enemy_image(enemies, walls):
+def construct_adversary_image(agent, enemies, walls):
     enemy_image = np.zeros(BOARD_HEIGHT, BOARD_WIDTH)
+    enemy_image[agent.position.x, agent.position.y] = 1
     for enemy in enemies:
-        enemy_image[enemy.position.x, enemy.position.y] = 1
+        enemy_image[enemy.position.x, enemy.position.y] = -1
     enemy_image_flatten= enemy_image.flatten()
     enemy_image_flatten = remove_walls_from_image(walls, enemy_image_flatten)
 
     return enemy_image_flatten
+
+def construct_powerup_vector(the_agent, enemies):
+    raise NotImplemented()
 
 def remove_walls_from_image(walls, image_flatten):
     new_image = image_flatten
